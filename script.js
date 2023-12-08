bea7469b13967eb38bb9e8c5b33075fd0b52bfa5
Aniket Khangar <116418804+aniket100200@users.noreply.github.com>
2023-12-08 13:19:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,30 @@
 const extendHex = (shortHex) => {
-  // write your code here
-};
+    // write your code here
+      // There may be hash present or not..
+      let size=shortHex.length; 
+  
+      //our starting point should be either 0 or 1 depending on use case
+      let i=size-3;
+      let ans="#";
+  
+      let isCapital=false;
+      //I'll have to check that if the perticular input contains the 
+      while(i<size)
+      {
+          let curr=shortHex[i];
+          let code=curr.charCodeAt();
 
-// Do not change the code below.
-const shortHex = prompt("Enter Short Hex.");
-alert(extendHex(shortHex));
+          if(code>=65 && code<=90) isCapital=true;
+          console.log(code,isCapital);
+         ans+=curr+curr;
+
+          i++;
+      }
+
+      return isCapital?ans.toUpperCase() : ans;
+  };
+  
+  // Do not change the code below.
+  const shortHex = prompt("Enter Short Hex.");
+  alert(extendHex(shortHex)); 
+  
